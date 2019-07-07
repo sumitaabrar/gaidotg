5a8cb7ae3391ea147e031c47d34aab64c73b860e
khadijakhan <khadijakhan@github.com>
2019-07-07 20:08:16 +0500

    signup and signin pages

---
+++ resources/views/inc/signup-in-header.blade.php
@@ -0,0 +1,14 @@
+<header class="topbar2 topbarCustom">
+    <nav class="navbar top-navbar navbar-toggleable-sm navbar-light">
+  
+      <!-- Logo -->
+      <div class="navbar-header">
+          <a class="navbar-brand" href="{{ route('index') }}">
+            <b> <img src="assets/images/logo-light-icon.png" alt="gaido" class="light-logo" /> </b>
+            <span> <img src="assets/images/logo-light-text.png" class="light-logo" alt="gaido" /> </span> 
+          </a>
+      </div>
+
+      </div>
+    </nav>
+  </header>
